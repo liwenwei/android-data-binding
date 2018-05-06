12d5f15f148d2fa6f20ea51564c827bee4aad863
liwenwei <liwenwei06@126.com>
2018-05-06 19:34:26 +0800

    Layouts and binding
    1. Enable dataBinding in your module level gradle
    2. Create a POJO class called Person
    3. Establish DataBinding

--- app/src/main/java/com/liwenwei/mydatabinding/MainActivity.java
+++ app/src/main/java/com/liwenwei/mydatabinding/MainActivity.java
@@ -1,13 +1,19 @@
 package com.liwenwei.mydatabinding;
 
+import android.databinding.DataBindingUtil;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import com.liwenwei.mydatabinding.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+
+        ActivityMainBinding binding = DataBindingUtil.setContentView(this, R.layout.activity_main);
+        User user = new User("Test", "User");
+        binding.setUser(user);
     }
 }
